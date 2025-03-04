3a3e17e4835bb5bf3eccf03c1634e1e5bea697ee
Tima <8vdead@gmail.com>
2025-03-04 17:06:02 +0500

    Initial commit

---
+++ app/src/main/java/com/example/lab02/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.lab02
+
+
+import android.content.ContentValues.TAG
+import android.os.Bundle
+import android.util.Log;
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.d(TAG, "kuku")
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart")
+    }
+}
